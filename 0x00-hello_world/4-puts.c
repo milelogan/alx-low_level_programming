129e5d72fa0efe88b082158c6c8bd54165df6800
okoene <36605483+milelogan@users.noreply.github.com>
2022-09-08 11:19:05 +0200

    updated

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -4,7 +4,8 @@
  *
  * Return: always 0
  */
-int man(void){
-	puts("Programming is like building a multilingual puzzle \n");
+int man(void)
+{
+	puts("\"Programming is like building a multilingual puzzle \n");
 	return(0);
 }
