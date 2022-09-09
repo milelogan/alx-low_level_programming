cd917634247c3fdb351bb8c37bb6925e93235091
milelogan <somtookoene65@gmail.com>
2022-09-09 02:58:42 -0700

    printing numbers using c for loop

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+/**
+* main - entry point
+*
+* Return: alway returns 0
+*
+*/
+
+int main(void)
+{
+	int numbers;
+
+	for (numbers = 0; numbers < 10; numbers++)
+		printf("%d", numbers);
+	return (0);
+}
