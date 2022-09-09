4fd4cba4a6c9212a938bb3ff75a32e987bb9a748
milelogan <somtookoene65@gmail.com>
2022-09-09 13:17:30 -0700

    omo

--- 0x01-variables_if_else_while/100-print_comb3.c
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -19,7 +19,7 @@ int main(void)
 					{
 					putchar((x % 10) + '0');
 					putchar((y % 10) + '0');
-					if(x != 8 || y != 9)
+					if (x != 8 || y != 9)
 					{
 					putchar(',');
 					putchar(' ');
