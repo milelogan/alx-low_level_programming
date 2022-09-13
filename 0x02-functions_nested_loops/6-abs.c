55879fbdabca94b1fdad84d7ef3b42db81e8a8cd
milelogan <somtookoene65@gmail.com>
2022-09-13 03:02:53 -0700

    to print absolute value of a number

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+ * _abs - main function
+ *
+ *r: is character to compare
+ *
+ * Return: always 0.
+ */
+
+int _abs(int r)
+{
+	(r > 0) ? return (r) : return (r * -1);
+	
+}
