2e1b436849b6dea1a49f837d565ed2f63dd6c83d
milelogan <somtookoene65@gmail.com>
2022-09-13 02:13:37 -0700

    adding main.h

---
+++ 0x02-functions_nested_loops/main.h
@@ -0,0 +1,48 @@
+#ifndef _MAIN_H_
+#define _MAIN_H_
+
+/**
+ * _putchar -writes the character c to std
+ * @c: parameter to compare
+ * Return: 1 or 0
+ */
+
+void print_alphabet(void);
+/**
+ * print_alphabet - function to print alphabet
+ *
+ */
+
+void print_alphabet_x10(void);
+/**
+ * print_alphabet_x10 - function to print alphabet 10 times
+ *
+ */
+
+int _islower(int c);
+/**
+ * _islower - function to print c in lowercase
+ * c is character to use
+ * Return 1 or 0
+ */
+
+int _isalpha(int c);
+/**
+ * _isalpha is a function to print if character is alphabet
+ * c is character of use
+ * Return 1 or 0
+ */
+
+int print_sign(int n);
+/**
+  * print_sign - prints a sign based on value of n
+  * n is integer input
+  * Return: 1 or 0 or -1
+  */
+
+
+
+
+
+#endif
+
