c09fe611f514542c02077dd552c1a85d4c905c54
milelogan <somtookoene65@gmail.com>
2022-09-13 16:19:09 -0700

    fibonacci sequence 2

---
+++ 0x02-functions_nested_loops/102-fibonacci.c
@@ -0,0 +1,33 @@
+#include <stdio.h>
+
+/**
+ * main - main function
+ *
+ * Return: always 0;
+ *
+ */
+
+int main(void)
+{
+	int counter = 2;
+	long int a = 1;
+	long int b = a + 1;
+	long int c = a + b;
+
+	printf("%ld, %ld, ", a, b);
+
+	while (counter < 50)
+	{
+		printf("%ld", c);
+		counter++;
+		a = b;
+		b = c;
+		c = a + b;
+		if (counter < 50)
+		{
+			printf(", ");
+		}
+	}
+	printf("%d\n", counter);
+	return (0);
+}
