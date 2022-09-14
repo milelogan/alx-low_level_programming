38724d82fcf6ca5ebb5f4057f3f8534e50fc9c1a
milelogan <somtookoene65@gmail.com>
2022-09-13 17:40:19 -0700

    fib abeg

--- 0x02-functions_nested_loops/104-fibonacci.c
+++ 0x02-functions_nested_loops/104-fibonacci.c
@@ -14,17 +14,26 @@ int main(void)
 	long int sum = 0;
 	long int num = 1;
 	long int num2 = 2;
-	long int r0, r1, r2, r3, r4, r5;
 
 	printf("%ld, ", num);
 	printf("%ld, ", num2);
 
-	for (x = 0; x < 96; x++)
+	for (x = 0; x < 92; x++)
 	{
 		sum = num + num2;
 		num = num2;
 		num2 = sum;
 		printf(", %ld", sum);
+
+		if (x != 96)
+		{
+			printf(",");
+			printf(" ");
+		} else if (x > 91) 
+		{
+			printf((sum / 1000000000));
+			printf((sum % 1000000000));
+		}
 	}
 
 
