dfb91eabcb3519e9f005a9ba9ca4627ab9903d23
milelogan <somtookoene65@gmail.com>
2022-09-14 00:14:48 -0700

    Learning how to debug in C

---
+++ 0x03-debugging/1-main.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+
+/**
+* main - causes an infinite loop
+*
+*/
+
+int main(void)
+{
+	int i;
+
+	print("Infinite loop incoming :(\n");
+
+	i = 0;
+
+	/** while (i < 10)
+	* {
+		* putchar(i);
+	*} */
+
+	printf("Infinite loop is avoided! \\o\n");
+
+	return (0);
+}
