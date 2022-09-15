f4ef13ad3ebe59b21737a72131f8d6969b986151
milelogan <somtookoene65@gmail.com>
2022-09-15 13:40:26 -0700

    prime factors

---
+++ 100-prime_factor.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+
+/**
+ * main - main function
+ *
+ * Return: void;
+ */
+
+int main(void)
+{
+	unsigned int x = 2;
+	unsigned long y = 612852475143;
+	
+	while (x != y)
+	{
+		if (y % x == 0)
+		{
+			y = y / x;
+		} else
+		{
+			x++;
+		}
+	}
+	printf("%lu\n", y);
+	return (0);
