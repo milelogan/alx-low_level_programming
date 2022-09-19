4ceeb0bedc2bc63f25f43828e48d98803172de20
milelogan <somtookoene65@gmail.com>
2022-09-19 15:52:34 +0200

    learning array destination

---
+++ 0x05-pointers_arrays_strings/9-strcpy.c
@@ -0,0 +1,22 @@
+#include "main.h"
+#include "2-strlen.c"
+
+/**
+ * _strcpy - main function
+ *
+ * @dest: destination
+ * @src: source
+ *
+ * Return: void
+ */
+
+char *_strcpy(char *dest, char *src)
+{
+	int k;
+
+	for (k = 0; k <= _strlen(src); i++)
+	{
+		dest[i] = src[i];
+	}
+	return (dest);
+}
