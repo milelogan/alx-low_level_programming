deb8f8185643d102135176c437666e3bfb1bc8d2
milelogan <somtookoene65@gmail.com>
2022-09-19 15:34:27 +0200

    task 7

---
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -0,0 +1,28 @@
+#include "main.h"
+#include "2-strlen.c"
+
+/**
+ * puts_half - main function
+ * @str: character
+ *
+ * Return: void
+ */
+
+void puts_half(char *str)
+{
+	int i;
+	int c = 0;
+	int lenn = _strlen(str)
+	int len = _strlen(str) % 2;
+
+	if (len != 0)
+	{
+		c += 1;
+	}
+
+	for (i = (lenn + c) /2; i < lenn; i++)
+	{
+		_putchar(str[i]);
+	}
+	_putchar('\n');
+}
