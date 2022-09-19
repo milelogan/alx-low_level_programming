04b06050822c69618e56178bc47f346f758d7f7f
milelogan <somtookoene65@gmail.com>
2022-09-19 09:46:34 +0200

    puts a string in reverse

---
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -0,0 +1,20 @@
+#include "main.h"
+#include "2-strlen.c"
+
+/**
+ * print_rev - main function
+ * @s: character
+ *
+ * Return: void
+ */
+
+void print_rev(char *s)
+{
+	int i;
+
+	for (i = _strlen(s); i >= 0; i++)
+	{
+		_putchar(*(s + i));
+	}
+	_putchar('\n');
+}
