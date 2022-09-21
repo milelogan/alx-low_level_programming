20122172ff0e121417cd292e6d91f3e82504b9e5
milelogan <somtookoene65@gmail.com>
2022-09-21 19:22:23 +0200

    to upper case

--- 0x06-pointers_arrays_strings/5-string_toupper.c
+++ 0x06-pointers_arrays_strings/5-string_toupper.c
@@ -10,13 +10,12 @@ char *string_toupper(char *s)
 {
 	int i;
 
-	while (s[i] != '\0')
+	for (i = 0; s[i] != '\0'; i++)
 	{
 		if (s[i] >= 'a' && s[i] <= 'z')
 		{
 			s[i] = s[i] - 32;
 		}
-		i++;
 	}
-	return (0);
+	return (s);
 }
