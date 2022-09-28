0fd83df1b9e3ac928906573fca4a1d986f905b99
milelogan <somtookoene65@gmail.com>
2022-09-28 10:43:16 +0200

    palindrome

---
+++ 0x08-recursion/100-is_palindrome.c
@@ -0,0 +1,39 @@
+#include "main.h"
+#include "2-strlen_recursion.c"
+
+/**
+ * helper - helps
+ * @a: okay
+ * @s: string
+ * Return: value
+ */
+
+int helper(int a, char *s)
+{
+	if (*s)
+	{
+		if (*s != s[_strlen_recursion(s) - a])
+		{
+			return (0);
+		}
+		else
+		{
+			return (helper(a + 1, s + 1));
+		}
+	}
+	return (1);
+}
+
+
+/**
+ * is_palindrome - function
+ * @s: string
+ * Return: value
+ */
+
+int is_palindrome(char *s)
+{
+	int i = 1;
+
+	return (helper(a, s));
+}
