3ff92aa36586f8abbbba1efb63feed1264424b3b
milelogan <somtookoene65@gmail.com>
2022-10-12 11:51:28 +0200

    correction on ;

--- 0x0F-function_pointers/1-array_iterator.c
+++ 0x0F-function_pointers/1-array_iterator.c
@@ -15,6 +15,6 @@ void array_iterator(int *array, size_t size, void (*action)(int))
 
 	if (array == NULL || action == NULL)
 		exit(0);
-	for (i = 0; i < size, i++)
+	for (i = 0; i < size; i++)
 		action(array[i]);
 }
