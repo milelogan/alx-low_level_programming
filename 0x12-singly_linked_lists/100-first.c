0d9806a6b9ef0712faa884a3c3fa85bd1f7f6767
milelogan <somtookoene65@gmail.com>
2022-10-20 12:48:56 +0200

    advanced task

---
+++ 0x12-singly_linked_lists/100-first.c
@@ -0,0 +1,13 @@
+#include "lists.h"
+#include <stdlib.h>
+#include <stdio.h>
+
+/**
+ * constructorRun - runs
+ *  before main function is executed.
+ */
+void constructorRun(void)
+{
+	printf("You're beat! and yet, you must allow,\n"
+		"I bore my house upon my back!\n");
+}
