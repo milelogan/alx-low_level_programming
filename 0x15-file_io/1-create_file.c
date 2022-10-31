e70e892302076d35e1564c7c3f51eaf37a93b321
milelogan <somtookoene65@gmail.com>
2022-10-31 16:42:13 +0100

    Tasks corrected

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -1,33 +1,57 @@
 #include "main.h"
 
 /**
- * create_file - Creates a file.
- * @filename: A pointer to the name of the file to create.
- * @text_content: A pointer to a string to write to the file.
- *
- * Return: If the function fails - -1.
- *         Otherwise - 1.
+ * _strlen - find length of string
+ * @str: string
+ * Return: length
  */
-int create_file(const char *filename, char *text_content)
+
+int _strlen(char *str)
 {
-	int fd, w, len;
+	int len;
 
-	if (filename == NULL)
-		return (-1);
-
-	if (!text_content)
-		text_content = "";
-
-	for (len = 0; *(text_content + len); len++)
+	for (len = 0; str[len] != '\0'; len++)
 		;
 
-	fd = open(filename, O_CREAT | O_RDWR | O_TRUNC, 0600);
-	w = write(fd, text_content, len);
+	return (len);
+}
 
-	if (fd == -1 || w == -1)
+/**
+ * create_file - creates file with permissions rw------- and writes content in
+ * if file already exists, don't change permissions and just truncate it
+ * @filename: name to give to new file
+ * @text_content: writes this content into file
+ * Return: 1 on success, -1 on error
+ */
+
+int create_file(const char *filename, char *text_content)
+{
+	int fd;
+	int n_wrote;
+
+	if (!filename)
 		return (-1);
 
-	close(fd);
+	/* create with permissions if file doesn't exist, else truncate */
+	fd = open(filename, O_CREAT | O_WRONLY | O_TRUNC, 0600);
+	if (fd == -1)
+		return (-1);
 
+	/* if nothing to write, just return newly created file */
+	if (!text_content)
+	{
+		close(fd);
+		return (1);
+	}
+
+	/* write */
+	n_wrote = write(fd, text_content, _strlen(text_content));
+	if (n_wrote == -1 || n_wrote != _strlen(text_content))
+	{
+		close(fd);
+		return (-1);
+	}
+
+	close(fd);
 	return (1);
 }
--- 0x15-file_io/2-append_text_to_file.c
+++ 0x15-file_io/2-append_text_to_file.c
@@ -1,34 +1,55 @@
 #include "main.h"
 
 /**
- * append_text_to_file - Appends text at the end of a file.
- * @filename: A pointer to the name of the file.
- * @text_content: The string to add to the end of the file.
- *
- * Return: If the function fails or filename is NULL - -1.
- *         If the file does not exist the user lacks write permissions - -1.
- *         Otherwise - 1.
+ * _strlen - find length of string
+ * @str: string
+ * Return: length
+ */
+
+int _strlen(char *str)
+{
+	int len;
+
+	for (len = 0; str[len] != '\0'; len++)
+		;
+
+	return (len);
+}
+
+/**
+ * append_text_to_file - append text and only if file exists
+ * @filename: file
+ * @text_content: appends this content into file
+ * Return: 1 on success, -1 on error
  */
 int append_text_to_file(const char *filename, char *text_content)
 {
-	int fd, w, len = 0;
+	int fd;
+	int n_wrote;
 
-	if (filename == NULL)
+	if (!filename)
 		return (-1);
 
-	if (text_content)
-	{
-		for (len = 0; text_content[len]; len++)
-		;
-		fd = open(filename, O_WRONLY | O_APPEND);
-		w = write(fd, text_content, len);
+	/* open file if it exists */
+	fd = open(filename, O_WRONLY | O_APPEND);
+	if (fd == -1)
+		return (-1);
 
-		if (fd == -1 || w == -1)
-			return (-1);
+	/* if nothing to write, still successful */
+	if (!text_content)
+	{
+		close(fd);
+		return (1);
+	}
+
+	/* write */
+	n_wrote = write(fd, text_content, _strlen(text_content));
+	if (n_wrote == -1 || n_wrote != _strlen(text_content))
+	{
+		close(fd);
+		return (-1);
 	}
 
 	close(fd);
-
 	return (1);
 }
-
