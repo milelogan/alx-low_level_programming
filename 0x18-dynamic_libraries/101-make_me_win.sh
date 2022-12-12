659ee719eb11301895b6443a3f4cec9b410aba2d
milelogan <somtookoene65@gmail.com>
2022-12-12 18:25:28 +0100

    tired

--- 0x18-dynamic_libraries/101-make_me_win.sh
+++ 0x18-dynamic_libraries/101-make_me_win.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-wget -P /tmp https://github.com/milelogan/alx-low_level_programming/0x18-dynamic_libraries/libmyprintf.so
+wget -P /tmp https://github.com/milelogan/alx-low_level_programming/raw/master/0x18-dynamic_libraries/libmyprintf.so
 export LD_PRELOAD=/tmp/libmyprintf.so
